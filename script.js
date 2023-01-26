9eb5d8adc41bf7d58a69d0bb8b79a9aa4528473f
Joshua Almodovar <joshuarosamktp4@gmail.com>
2023-01-25 20:10:59 -0600

    Fetch quote from api

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-window.console.log("Hello World!");
+let apiQuotes = [];
+
+//Show new quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get quotes From API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error here
+  }
+}
+
+// On load
+getQuotes();
